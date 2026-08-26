b58e13279996d64eaf4def03783ccf180280b996
Devchauhan108 <dcrd108@gmail.com>
2026-08-26 10:45:56 +0530

    Add code editor and run submit functionality

--- vite-project/src/pages/ProblemDetail/ProblemDetail.jsx
+++ vite-project/src/pages/ProblemDetail/ProblemDetail.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"; 
+import { useState } from "react";
 import problems from "../../data/problems"; 
 import "./ProblemDetail.css"; 
 function ProblemDetail(){ 
@@ -15,6 +16,43 @@ function ProblemDetail(){
             "    }", 
             "}" 
         ]; 
+        const [code, setCode] = useState(codeLines.join("\n"));
+        const [output, setOutput] = useState("");
+        const [status, setStatus] = useState("");
+        const [input, setInput] = useState("");
+        const [target, setTarget] = useState("");
+     const handleRun = () => {
+    setStatus("");
+
+    if (input === "") {
+        setOutput("Please enter input");
+        return;
+    }
+
+    const nums = input.split(" ").map(Number);
+    const targetNumber = Number(target);
+
+    let answer = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] + nums[j] === targetNumber) {
+                answer = [i, j];
+            }
+
+        }
+    }
+
+    if (answer.length > 0) {
+        setOutput(`[${answer[0]}, ${answer[1]}]`);
+    } else {
+        setOutput("No solution");
+    }
+};
+        const handleSubmit = () => {
+            setStatus("Accepted");
+        };
     return( 
      <div className="problem-detail"> 
         <div className="problem-content"> 
@@ -44,34 +82,73 @@ function ProblemDetail(){
             })} 
          </ul> 
          </div> 
-         <div className="code-section"> 
-            <div className="editor-header"> 
-                Java 
-            </div> 
-            <div className="code-area">
-    <pre>
-        <code>
-            {codeLines.map((line, index) => {
-                return (
-                    <span className="code-line" key={index}>
-                        <span className="line-number">
-                            {index + 1}
-                        </span>
+        <div className="code-section">
 
-                        <span className="line-code">
-                            {line}
-                        </span>
-                    </span>
+    <div className="editor-header">
+        Java
+    </div>
+
+    <div className="code-area">
+
+        <div className="line-numbers">
+            {code.split("\n").map((line, index) => {
+                return (
+                    <div key={index}>
+                        {index + 1}
+                    </div>
                 );
             })}
-        </code>
-    </pre>
-</div> 
-               
- 
- 
-                     
-</div> 
-    </div> 
+        </div>
+
+        <textarea
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+        />
+
+    </div>
+    <div className="input-section">
+    <h3>Custom Input</h3>
+
+    <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter your input..."
+    />
+    </div>
+  <div className="editor-actions">
+
+    <button onClick={handleRun}>
+        Run
+    </button>
+
+    <button onClick={handleSubmit}>
+        Submit
+    </button>
+
+</div>
+<div className="input-section">
+    <h3>Target</h3>
+
+    <input
+        value={target}
+        onChange={(e) => setTarget(e.target.value)}
+        placeholder="Enter target..."
+    />
+</div>
+
+{status && (
+    <div className="status-section">
+        <h3>{status}</h3>
+    </div>
 )}
+        {output && (
+        <div className="output-section">
+            <h3>Output</h3>
+            <pre>{output}</pre>
+        </div>
+    )}
+
+</div>
+</div>
+    )}
 export default ProblemDetail;
